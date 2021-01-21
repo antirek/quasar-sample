2e2f36ef4269366a7b4efd84e5f8c7e6346bf01e
Sergey <serge.dmitriev@gmail.com>
2021-01-21 15:49:14 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const config = require('config');
 const cors = require('cors');
-const quasarServe = require('./quasar-serve');
+// const quasarServe = require('./quasar-serve');
+const quasarServe = require('quasar-serve');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.use('/', quasarServe({
-  distPath: './../client/dist/spa',
+app.use(quasarServe({
+  distPath: path.join(__dirname, './../client/dist/spa'),
   replaces: {
     baseUrl: config.baseUrl,
-  }
+  },
 }));
 
 app.listen(config.port, () => {
